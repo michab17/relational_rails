544522f23e1c5ae407c7db48f3315c465208f393
Micha Bernhard <mib70@pitt.edu>
2021-10-12 15:56:53 -0600

    Make hotels equal to an instance variable

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -6,10 +6,14 @@ class HotelsController < ApplicationController
   def new
   end
 
+  def show
+    @hotel = Hotel.find(params[:id])
+  end
+
   def create
     hotel = Hotel.new({
       name: params[:hotel][:name],
-      vacancy?: params[:hotel][:vacancy?],
+      vacancy: params[:hotel][:vacancy],
       occupancy: params[:hotel][:occupancy],
       opening_day: params[:hotel][:opening_day],
       newest_check_in: params[:hotel][:newest_check_in]
