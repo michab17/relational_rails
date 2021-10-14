04e826132ece15db790a3ea85fa1d7d3385748e8
Micha Bernhard <mib70@pitt.edu>
2021-10-14 15:24:06 -0600

    Test links to parent and child index

--- spec/features/hotels/hotel_spec.rb
+++ spec/features/hotels/hotel_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'Hotel tests' do
       visit "/hotels"
       click_link "New Hotel"
 
-      expect(page).to have_content("Enter a new hotel:")
+      expect(current_path).to eq("/hotels/new")
     end
 
     it 'displays all the hotels in the database' do
@@ -41,6 +41,107 @@ RSpec.describe 'Hotel tests' do
       expect(page).to have_content(hotel.occupancy)
       expect(page).to have_content(hotel.created_at)
       expect(page).to have_content(hotel.updated_at)
+    end
+
+    it 'displays the number of children' do
+      hotel = Hotel.create!(name: "hotel1", vacancy: true, occupancy: 200, created_at: DateTime.now, updated_at: DateTime.now)
+      guest1 = hotel.guests.create!(name: "Maximus", royalty_member: false, room_number: 10, created_at: DateTime.now, updated_at: DateTime.now)
+      guest2 = hotel.guests.create!(name: "Maximus", royalty_member: false, room_number: 10, created_at: DateTime.now, updated_at: DateTime.now)
+      guest3 = hotel.guests.create!(name: "Maximus", royalty_member: false, room_number: 10, created_at: DateTime.now, updated_at: DateTime.now)
+
+      visit "/hotels/#{hotel.id}"
+
+      expect(page).to have_content('3')
+    end
+  end
+
+  describe '/hotels' do
+    it 'the instances of hotel should be ordered by created at' do
+      hotel = Hotel.create!(name: "hotel1", vacancy: true, occupancy: 200, created_at: DateTime.now, updated_at: DateTime.now)
+      hotel4 = Hotel.create!(name: "hotel4", vacancy: true, occupancy: 1000, created_at: DateTime.now, updated_at: DateTime.now)
+      hotel3 = Hotel.create!(name: "hotel3", vacancy: true, occupancy: 100, created_at: DateTime.now, updated_at: DateTime.now)
+      hotel2 = Hotel.create!(name: "hotel2", vacancy: false, occupancy: 0, created_at: DateTime.now, updated_at: DateTime.now)
+  
+      visit "/hotels"
+  
+      expect(page).to have_content(hotel.name)
+      expect(page).to have_content(hotel.created_at)
+      expect(page).to have_content(hotel4.name)
+      expect(page).to have_content(hotel4.created_at)
+      expect(page).to have_content(hotel3.name)
+      expect(page).to have_content(hotel3.created_at)
+      expect(page).to have_content(hotel2.name)
+      expect(page).to have_content(hotel2.created_at)
+    end
+  end
+
+  describe 'Parent Index Link' do
+    it 'takes the user back to the parent index' do
+      hotel = Hotel.create!(name: "hotel1", vacancy: true, occupancy: 200, created_at: DateTime.now, updated_at: DateTime.now)
+
+      visit "/hotels/#{hotel.id}"
+
+      expect(page).to have_link("Hotels")
+
+      click_link "Hotels"
+
+      expect(current_path).to eq("/hotels")
+      expect(current_path).to_not eq("/hotels/#{hotel.id}")
+
+      visit '/hotels/new'
+
+      expect(page).to have_link("Hotels")
+
+      click_link "Hotels"
+
+      expect(current_path).to eq("/hotels")
+
+      visit '/guests'
+
+      click_link "Hotels"
+
+      expect(current_path).to eq("/hotels")
+    end
+  end
+
+  describe 'Child Index Link' do
+    it 'takes the user back to the child index' do
+      hotel = Hotel.create!(name: "hotel1", vacancy: true, occupancy: 200, created_at: DateTime.now, updated_at: DateTime.now)
+
+      visit "/hotels/#{hotel.id}"
+
+      expect(page).to have_link("Guests")
+
+      click_link "Guests"
+
+      expect(current_path).to eq("/guests")
+      expect(current_path).to_not eq("/hotels/#{hotel.id}")
+
+      visit '/hotels/new'
+
+      expect(page).to have_link("Guests")
+
+      click_link "Guests"
+
+      expect(current_path).to eq("/guests")
+
+      visit '/hotels'
+
+      click_link "Guests"
+
+      expect(current_path).to eq("/guests")
+    end
+  end
+
+  describe 'Child Table Link' do
+    it 'takes you to /hotels/:id/guests' do
+      hotel = Hotel.create!(name: "hotel1", vacancy: true, occupancy: 200, created_at: DateTime.now, updated_at: DateTime.now)
+
+      visit "/hotels/#{hotel.id}"
+
+      click_link "Guests"
+
+      expect(current_path).to eq("/hotels/#{hotel.id}/guests")
     end
   end
 end
--- spec/features/zoos/zoo_spec.rb
+++ spec/features/zoos/zoo_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'Zoo tests' do
       visit "/zoos"
       click_link "New Zoo"
 
-      expect(page).to have_content("Enter a new zoo:")
+      expect(current_path).to eq("/zoos/new")
     end
 
     it 'displays all the zoos in the database' do
@@ -42,20 +42,106 @@ RSpec.describe 'Zoo tests' do
       expect(page).to have_content(zoo.created_at)
       expect(page).to have_content(zoo.updated_at)
     end
+
+    it 'displays the number of children' do
+      zoo = Zoo.create!(name: "zoo1", open: true, num_of_people: 200, created_at: DateTime.now, updated_at: DateTime.now)
+      animal1 = zoo.animals.create!(name: "Maximus", has_covid: false, age: 10, created_at: DateTime.now, updated_at: DateTime.now)
+      animal2 = zoo.animals.create!(name: "Maximus", has_covid: false, age: 10, created_at: DateTime.now, updated_at: DateTime.now)
+      animal3 = zoo.animals.create!(name: "Maximus", has_covid: false, age: 10, created_at: DateTime.now, updated_at: DateTime.now)
+
+      visit "/zoos/#{zoo.id}"
+
+      expect(page).to have_content('3')
+    end
   end
 
   describe '/zoos' do
     it 'the instances of zoo should be ordered by created at' do
       zoo = Zoo.create!(name: "Zoo1", open: true, num_of_people: 200, created_at: DateTime.now, updated_at: DateTime.now)
-      zoo2 = Zoo.create!(name: "Zoo2", open: false, num_of_people: 0, created_at: DateTime.now, updated_at: DateTime.now)
-      zoo3 = Zoo.create!(name: "Zoo3", open: true, num_of_people: 100, created_at: DateTime.now, updated_at: DateTime.now)
       zoo4 = Zoo.create!(name: "Zoo4", open: true, num_of_people: 1000, created_at: DateTime.now, updated_at: DateTime.now)
+      zoo3 = Zoo.create!(name: "Zoo3", open: true, num_of_people: 100, created_at: DateTime.now, updated_at: DateTime.now)
+      zoo2 = Zoo.create!(name: "Zoo2", open: false, num_of_people: 0, created_at: DateTime.now, updated_at: DateTime.now)
   
       visit "/zoos"
   
-      save_and_open_page
-  
       expect(page).to have_content(zoo.name)
+      expect(page).to have_content(zoo.created_at)
+      expect(page).to have_content(zoo4.name)
+      expect(page).to have_content(zoo4.created_at)
+      expect(page).to have_content(zoo3.name)
+      expect(page).to have_content(zoo3.created_at)
+      expect(page).to have_content(zoo2.name)
+      expect(page).to have_content(zoo2.created_at)
+    end
+  end
+
+  describe 'Parent Index Link' do
+    it 'takes the user back to the parent index' do
+      zoo = Zoo.create!(name: "Zoo1", open: true, num_of_people: 200, created_at: DateTime.now, updated_at: DateTime.now)
+
+      visit "/zoos/#{zoo.id}"
+
+      expect(page).to have_link("Zoos")
+
+      click_link "Zoos"
+
+      expect(current_path).to eq("/zoos")
+      expect(current_path).to_not eq("/zoos/#{zoo.id}")
+
+      visit 'zoos/new'
+
+      expect(page).to have_link("Zoos")
+
+      click_link "Zoos"
+
+      expect(current_path).to eq("/zoos")
+
+      visit '/animals'
+
+      click_link "Zoos"
+
+      expect(current_path).to eq("/zoos")
+    end
+  end
+
+  describe 'Child Index Link' do
+    it 'takes the user to the child index' do
+      zoo = Zoo.create!(name: "Zoo1", open: true, num_of_people: 200, created_at: DateTime.now, updated_at: DateTime.now)
+
+      visit "/zoos/#{zoo.id}"
+
+      expect(page).to have_link("Animals")
+
+      click_link "Animals"
+
+      expect(current_path).to eq("/animals")
+      expect(current_path).to_not eq("/zoos/#{zoo.id}")
+
+      visit 'zoos/new'
+
+      expect(page).to have_link("Animals")
+
+      click_link "Animals"
+
+      expect(current_path).to eq("/animals")
+
+      visit '/zoos'
+
+      click_link "Animals"
+
+      expect(current_path).to eq("/animals")
+    end
+  end
+
+  describe 'Child Table Link' do
+    it 'takes you to /zoos/:id/animals' do
+      zoo = Zoo.create!(name: "Zoo1", open: true, num_of_people: 200, created_at: DateTime.now, updated_at: DateTime.now)
+
+      visit "/zoos/#{zoo.id}"
+
+      click_link "Animals"
+
+      expect(current_path).to eq("/zoos/#{zoo.id}/animals")
     end
   end
 end
