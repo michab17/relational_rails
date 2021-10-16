acaacd6224875b8cd62a01ce1cdc8fa50adee4a4
Micha Bernhard <mib70@pitt.edu>
2021-10-15 21:52:42 -0600

    Write only_true tests

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Animal, type: :model do 
   it {should belong_to :zoo}
+
+  describe '#only_true' do
+    it 'only displays animals with a true boolean' do
+      zootopia = Zoo.create!(name: "Zootopia", open: true, num_of_people: 100)
+      fred = zootopia.animals.create!(name: "Fred", has_covid: false, age: 20)
+      prometheus = zootopia.animals.create!(name: "Prometheus", has_covid: true, age: 109)
+      
+      visit '/animals'
+      
+      expect(page).to have_content("Prometheus")
+      expect(page).to_not have_content("Fred")
+    end
+  end
+
 end 
