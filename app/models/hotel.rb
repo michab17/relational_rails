82c914da60ef0b225e38101a534fe74d633613aa
Micha Bernhard <mib70@pitt.edu>
2021-10-15 21:50:46 -0600

    Write sorting methods

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,11 @@
 class Hotel < ApplicationRecord
   has_many :guests
+
+  def self.reverse_order
+    Hotel.order("created_at").reverse
+  end
+
+  def self.sort_by_name
+    Hotel.animals.order("name")
+  end
 end
