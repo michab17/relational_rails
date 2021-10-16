60edf7eba15a9894fb6579a20fc01a956f3529d7
Micha Bernhard <mib70@pitt.edu>
2021-10-15 21:53:25 -0600

    Write create and update tests

--- spec/features/animals/animal_spec.rb
+++ spec/features/animals/animal_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'animal index' do 
   it 'shows the attributes of all the animals' do 
-    zoo = Zoo.create!(name: "Zoo1", open: true, num_of_people: 200, created_at: DateTime.now, updated_at: DateTime.now)
-    animal = zoo.animals.create!(name: "Fred", has_covid: false, age: 10, created_at: DateTime.now, updated_at: DateTime.now)
+    zoo = Zoo.create!(name: "Zoo1", open: true, num_of_people: 200)
+    animal = zoo.animals.create!(name: "Fred", has_covid: false, age: 10)
   
     visit "/animals"
 
@@ -12,5 +12,54 @@ RSpec.describe 'animal index' do
     expect(page).to have_content(animal.age)
     expect(page).to have_content(animal.created_at)
     expect(page).to have_content(animal.updated_at)
-  end 
+  end
+
+  describe 'Create Child' do
+    it 'creates a new child for given parent' do
+      zoo = Zoo.create!(name: "Zoo1", open: true, num_of_people: 200)
+  
+      visit "/zoos/#{zoo.id}/animals"
+
+      expect(page).to have_link("Create Animal")
+
+      click_link "Create Animal"
+
+      expect(current_path).to eq("/zoos/#{zoo.id}/animals/new")
+      expect(page).to have_button("Create Animal")
+
+      fill_in 'name', with: 'Fred'
+      check 'has_covid'
+      fill_in 'age', with: '10'
+
+      click_button "Create Animal"
+
+      expect(current_path).to eq("/zoos/#{zoo.id}/animals")
+      expect(page).to have_content("Fred")
+    end
+  end
+
+  describe 'Update' do
+    it 'updates the childs attributes' do
+      zoo = Zoo.create!(name: "Zoo1", open: true, num_of_people: 200)
+      animal = zoo.animals.create!(name: "Fred", has_covid: false, age: 10)
+  
+      visit "/animals/#{animal.id}"
+
+      expect(page).to have_link("Update Animal")
+
+      click_link "Update Animal"
+
+      expect(current_path).to eq("/animals/#{animal.id}/edit")
+
+      fill_in 'animal[name]', with: 'Elizabeth'
+      check 'animal[has_covid]'
+      fill_in 'animal[age]', with: '8'
+
+      click_button "Update Animal"
+
+      expect(current_path).to eq("/animals/#{animal.id}")
+      expect(page).to have_content('Elizabeth')
+      expect(page).to have_content('8')
+    end
+  end
 end 
--- spec/features/guests/guest_spec.rb
+++ spec/features/guests/guest_spec.rb
@@ -13,4 +13,53 @@ RSpec.describe 'guest index' do
     expect(page).to have_content(guest.created_at)
     expect(page).to have_content(guest.updated_at)
   end 
+
+  describe 'Create Child' do
+    it 'creates a new child for given parent' do
+      hotel = Hotel.create!(name: "hotel1", vacancy: true, occupancy: 200)
+  
+      visit "/hotels/#{hotel.id}/guests"
+
+      expect(page).to have_link("Create Guest")
+
+      click_link "Create Guest"
+
+      expect(current_path).to eq("/hotels/#{hotel.id}/guests/new")
+      expect(page).to have_button("Create Guest")
+
+      fill_in 'name', with: 'Fred'
+      check 'royalty_member'
+      fill_in 'room_number', with: '10'
+
+      click_button "Create Guest"
+
+      expect(current_path).to eq("/hotels/#{hotel.id}/guests")
+      expect(page).to have_content("Fred")
+    end
+  end
+
+  describe 'Update' do
+    it 'updates the childs attributes' do
+      hotel = Hotel.create!(name: "hotel1", vacancy: true, occupancy: 25)
+      guest = hotel.guests.create!(name: "Maximus", royalty_member: false, room_number: 10)
+      
+      visit "/guests/#{guest.id}"
+
+      expect(page).to have_link("Update Guest")
+
+      click_link "Update Guest"
+
+      expect(current_path).to eq("/guests/#{guest.id}/edit")
+
+      fill_in 'guest[name]', with: 'Fred'
+      check 'guest[royalty_member]'
+      fill_in 'guest[room_number]', with: '144'
+
+      click_button "Update Guest"
+
+      expect(current_path).to eq("/guests/#{guest.id}")
+      expect(page).to have_content('Fred')
+      expect(page).to have_content('144')
+    end
+  end
 end 
